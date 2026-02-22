103250caa0e346cf5b7bb8e6e4b5fb9024ae17fd
Kitsune <KyuubiRan@outlook.com>
2026-02-22 23:38:39 +0800

    fix: hint on ModPotential

--- src/Impl/Feats/FtNineTailFox.cs
+++ src/Impl/Feats/FtNineTailFox.cs
@@ -9,13 +9,13 @@ public class FtNineTailFox : BaseNTFFeat
     {
         ModBase(AbilityMagicShield.Instance.id, v, false, hint);
 
-        ModPotential(SKILL.STR, v * 11);
-        ModPotential(SKILL.END, v * 11);
-        ModPotential(SKILL.DEX, v * 11);
-        ModPotential(SKILL.PER, v * 11);
-        ModPotential(SKILL.LER, v * 11);
-        ModPotential(SKILL.WIL, v * 11);
-        ModPotential(SKILL.MAG, v * 11);
-        ModPotential(SKILL.CHA, v * 11);
+        ModPotential(SKILL.STR, v * 11, hint);
+        ModPotential(SKILL.END, v * 11, hint);
+        ModPotential(SKILL.DEX, v * 11, hint);
+        ModPotential(SKILL.PER, v * 11, hint);
+        ModPotential(SKILL.LER, v * 11, hint);
+        ModPotential(SKILL.WIL, v * 11, hint);
+        ModPotential(SKILL.MAG, v * 11, hint);
+        ModPotential(SKILL.CHA, v * 11, hint);
     }
 }
